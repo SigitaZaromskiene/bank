4acb9c996afa5ffe102dbef233e140e2ddd09829
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-03-14 17:22:07 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import Nav from "./components/Nav";
 import { GlobalProvider } from "./components/Global";
 
 import Routes from "./components/Routes";
-import CookieMonster from "./components/CookieMonster";
 
 function App() {
   return (
     <GlobalProvider>
-      <CookieMonster></CookieMonster>
       <Nav className="nav" hover="hover"></Nav>
 
       <Routes btn="button"></Routes>
